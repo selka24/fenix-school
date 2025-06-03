3e5f3706e4c2d9b669804cb09e576519d28bd32a
selka <krisel_hysenaj@hotmail.com>
2025-06-04 01:54:57 +0200

    test 3

--- src/core/pageBanners.ts
+++ src/core/pageBanners.ts
@@ -1,6 +1,5 @@
 import ActivityBanner from '/src/assets/images/activities/Fenix.jpg?w=400;800;1200;1920&format=webp&quality=100&as=srcset'
 import ClubBanner from '/src/assets/images/clubs/club.jpg?w=400;800;1200;1920&format=webp&quality=100&as=srcset'
-import CurriculumBanner from '/src/assets/images/curriculum/kurrikula.jpg?w=400;800;1200;1920&format=webp&quality=100&as=srcset'
 import AbroadBanner from '/src/assets/images/external/wood_farm_school.jpg?w=400;800;1200;1920&format=webp&quality=100&as=srcset'
 import AlBanner from '/src/assets/images/internal/internal.jpg?w=400;800;1200;1920&format=webp&quality=100&as=srcset'
 import ParentsBanner from '/src/assets/images/parents/parents.jpg?w=400;800;1200;1920&format=webp&quality=100&as=srcset'
@@ -8,6 +7,7 @@ import PrimaryBanner from '/src/assets/images/primary/primary.jpg?w=400;800;1200
 import SecondaryBanner from '/src/assets/images/secondary/secondary.jpg?w=400;800;1200;1920&format=webp&quality=100&as=srcset'
 import ProggresBanner from '/src/assets/images/progress/progress.jpg?w=400;800;1200;1920&format=webp&quality=100&as=srcset'
 import DefaultBanner from '/src/assets/images/hero/Shkolla Fenix 1.jpg?w=400;800;1200;1920&format=webp&quality=100&as=srcset'
+import CurriculumBanner from '/src/assets/images/programmes/programme.webp?w=400;800;1200;1920&format=webp&quality=100&as=srcset'
 
 export const pageBanners = {
   activities: ActivityBanner,
